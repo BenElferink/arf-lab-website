a19a5d1901f17e6150bcfbf6b6e6dceee000b3c1
Ben Elferink <ben.elferink@icloud.com>
2023-09-23 21:34:53 +0300

    edit team

--- components/Team.tsx
+++ components/Team.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image'
 import Glitch from './Glitch'
 import SocialIcon from './SocialIcon'
 
-const TeamCard = (props: {
+interface TeamMember {
   profilePicture: string
   name: string
   titles: string[]
   description: string
   socials: string[]
-}) => {
+}
+
+const TeamCard = (props: TeamMember) => {
   const { profilePicture, name, titles, description, socials } = props
 
   return (
@@ -29,13 +31,7 @@ const TeamCard = (props: {
 
       <div className='flex mt-auto'>
         {socials.map((url) => (
-          <SocialIcon
-            key={`social-${url}`}
-            url={url}
-            color='#fff'
-            size='w-6 h-6'
-            className='p-1 rounded-lg hover:bg-zinc-500'
-          />
+          <SocialIcon key={`social-${url}`} url={url} color='#fff' size='w-6 h-6' className='p-1 rounded-lg hover:bg-zinc-500' />
         ))}
       </div>
     </article>
@@ -50,38 +46,32 @@ const Team = () => {
       </div>
 
       <div className='flex flex-wrap justify-center max-w-7xl'>
-        {[
-          {
-            name: 'Riicoo90 (Ricardo Kind)',
-            titles: ['Founder'],
-            description: 'NFT enthusiast, crypto investor, and has a lot of love for the Anatomy community',
-            profilePicture: '/media/team/riicoo90.jpg',
-            socials: ['https://x.com/riicoo90', 'https://www.youtube.com/@Riicoo90'],
-          },
-          {
-            name: 'Psaeychi',
-            titles: ['Community Manager', 'Graphic Designer'],
-            description: 'CNFT Enthusiast, father of 3 children',
-            profilePicture: '/media/team/psaeychi.png',
-            socials: ['https://x.com/psaeychicnft'],
-          },
-          {
-            name: 'Ben',
-            titles: ['Fullstack Developer'],
-            description:
-              'I started my career as Fullstack Developer in 2020 & have been involved in the crypto & NFT space since 2021.',
-            profilePicture: '/media/team/ben.jpg',
-            socials: ['https://x.com/BenElferink', 'https://github.com/BenElferink'],
-          },
-        ].map(({ profilePicture, name, titles, description, socials }) => (
-          <TeamCard
-            key={`team-${name}`}
-            profilePicture={profilePicture}
-            name={name}
-            titles={titles}
-            description={description}
-            socials={socials}
-          />
+        {(
+          [
+            {
+              name: 'Riicoo90 (Ricardo Kind)',
+              titles: ['Founder'],
+              description: 'NFT enthusiast, crypto investor, and has a lot of love for the Anatomy community',
+              profilePicture: '/media/team/riicoo90.jpg',
+              socials: ['https://x.com/riicoo90', , 'https://discord.com/users/698650605677314108', 'https://www.youtube.com/@Riicoo90'],
+            },
+            {
+              name: 'Psaeychi',
+              titles: ['Community Manager', 'Graphic Designer'],
+              description: 'CNFT Enthusiast, father of 3 children',
+              profilePicture: '/media/team/psaeychi.png',
+              socials: ['https://x.com/psaeychicnft', 'https://discord.com/users/881884080445222933'],
+            },
+            {
+              name: 'Ben',
+              titles: ['Fullstack Developer'],
+              description: 'I started my career as Fullstack Developer in 2020 & have been involved in the crypto & NFT space since 2021.',
+              profilePicture: '/media/team/ben.png',
+              socials: ['https://x.com/BenElferink', 'https://discord.com/users/791763515554922507', 'https://github.com/BenElferink'],
+            },
+          ] as TeamMember[]
+        ).map(({ profilePicture, name, titles, description, socials }) => (
+          <TeamCard key={`team-${name}`} profilePicture={profilePicture} name={name} titles={titles} description={description} socials={socials} />
         ))}
       </div>
     </div>
