3f7139176caaf672f83fe389ede02882a5143392
Ben Elferink <ben.elferink@icloud.com>
2023-09-24 15:24:43 +0300

    less margin

--- components/Team.tsx
+++ components/Team.tsx
@@ -40,7 +40,7 @@ const TeamCard = (props: TeamMember) => {
 
 const Team = () => {
   return (
-    <div id='team' className='mt-40 mb-40'>
+    <div id='team' className='mt-40 mb-20'>
       <div className='flex items-center justify-center'>
         <Glitch>Team</Glitch>
       </div>
