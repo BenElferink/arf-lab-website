dd25f527992dfdb33534e80eff351847a3fc7b6b
Ben Elferink <ben.elferink@icloud.com>
2024-10-20 19:00:29 +0300

    fix build error (kill unused API)

---
+++ pages/api/index.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const config = {
+  maxDuration: 300,
+  api: {
+    responseLimit: false,
+  },
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req
+
+  try {
+    switch (method) {
+      case 'GET': {
+        return res.status(200).json({
+          message: 'Nothing to see here :)',
+        })
+      }
+
+      default: {
+        res.setHeader('Allow', 'GET')
+        return res.status(405).end()
+      }
+    }
+  } catch (error) {
+    console.error(error)
+    return res.status(500).end()
+  }
+}
+
+export default handler
