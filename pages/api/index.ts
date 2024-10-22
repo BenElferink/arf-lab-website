cc7aa6d74811dc8f868a625cd947c70b7ba79d59
Ben Elferink <ben.elferink@icloud.com>
2024-10-22 22:02:51 +0300

    fix: build error

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,32 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export const config = {
-  maxDuration: 300,
-  api: {
-    responseLimit: false,
-  },
-}
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method } = req
+  const { method } = req;
 
   try {
     switch (method) {
       case 'GET': {
         return res.status(200).json({
           message: 'Nothing to see here :)',
-        })
+        });
       }
 
       default: {
-        res.setHeader('Allow', 'GET')
-        return res.status(405).end()
+        res.setHeader('Allow', 'GET');
+        return res.status(405).end();
       }
     }
   } catch (error) {
-    console.error(error)
-    return res.status(500).end()
+    console.error(error);
+    return res.status(500).end();
   }
-}
+};
 
-export default handler
+export default handler;
